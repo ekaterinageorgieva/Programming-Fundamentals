9400c85eb1b4e23e864b5a67115bae8668a32846
Ekaterina Georgieva <147399489+ekaterinageorgieva@users.noreply.github.com>
2024-01-18 15:14:57 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿//using System;
+
+//namespace demo
+//{
+//    internal class Program
+//    {
+//        static void Main(string[] args)
+//        {
+//            int dollars = int.Parse(Console.ReadLine());
+//            double euro = dollars * 0.883795087;
+//            Console.WriteLine("Euro: " + euro);
+//        }
+//    }
+//}
+using System;
+
+double inches = double.Parse(Console.ReadLine());
+double cm = inches * 2.45;
+Console.WriteLine("Cm: " + cm);
